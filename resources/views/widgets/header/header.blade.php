c51b304ef0565cd9f7c9a70f40f9ca135ae35adf
Igor Kozhevnikov <igor.kozhevnikov@mireon.ru>
2020-07-20 18:22:18 +0300

    Develop | SF.

--- resources/views/widgets/header/header.blade.php
+++ resources/views/widgets/header/header.blade.php
@@ -12,7 +12,7 @@ use Mireon\SlidePanels\Widgets\Header\Header;
 
 @php($specialClass = $header->hasKey() ? "slide-panels__header-{$header->getKey()}" : '')
 
-<div class="slide-panels__header slide-panels__header_{{ $header->getSize() }} ?> {{ $specialClass }}">
+<div class="slide-panels__header slide-panels__header_{{ $header->getSize() }} {{ $specialClass }}">
     @if($header->hasIcon())
         <i class="slide-panels__header__icon slide-panels__header__icon_{{ $header->getSize() }} {{ $header->getIcon() }}"></i>
     @endif
